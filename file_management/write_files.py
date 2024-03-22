7e8659b26b5fc97f9ad161e19314d07084b3d017
hugocnl11 <hugocnl11@gmail.com>
2024-03-22 23:40:20 +0100

    0.4

---
+++ file_management/write_files.py
@@ -0,0 +1,5 @@
+text = "Have a nice day! See ya"
+
+
+with open('text.txt', 'w') as file:
+    file.write(text)
--- string/string_format.py
+++ string/string_format.py
@@ -22,3 +22,15 @@ print("Hello, my name is {:10} Nice to meet you".format(name))
 print("Hello, my name is {:<10} Nice to meet you".format(name))
 print("Hello, my name is {:>10} Nice to meet you".format(name))
 print("Hello, my name is {:^10} Nice to meet you".format(name))
+
+# how to format numbers:
+
+number = 3.141159
+number2 = 1000
+print("The number pi is {:.3f}".format(number)) #hace que muestres el numero de decimales que pongas en la x (.xf)
+print("The number is {:,}".format(number2)) # How to add a comma
+print("The number is {:b}".format(number2)) # Bimnary number
+print("The number is {:o}".format(number2)) # Octal number
+print("The number is {:x}".format(number2)) # Hexadecimal lowercase
+print("The number is {:X}".format(number2)) # Hexadecimal uppercase
+print("The number is {:E}".format(number2)) # Sientific notation
